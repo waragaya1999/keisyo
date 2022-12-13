f9019b3bbe4b19efdb617e99570a6aa01aa4929e
mochi <mochimochidaifucu@icloud.com>
2022-12-13 09:25:03 +0900

    Revert "不要ファイルの削除"
    This reverts commit 84334ecf321ac7870348a517a89d3c0221baf469.

---
+++ src/types/LocationDto.tsx
@@ -0,0 +1,6 @@
+export type LocationDto = {
+  straight: boolean
+  corner: boolean
+  specific: boolean
+  unconditional: boolean
+}
---
+++ src/types/mileageDto.tsx
@@ -0,0 +1,6 @@
+export type MileageDto = {
+  finalStage: boolean
+  final: boolean
+  secondHalf: boolean
+  middle: boolean
+}
