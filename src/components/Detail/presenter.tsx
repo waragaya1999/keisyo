2b8d6f407e06baed0d70884e7188d301c0fadaa3
mochi <85915891+waragaya1999@users.noreply.github.com>
2023-01-31 22:29:32 +0900

    仮ビルド

--- src/components/Detail/presenter.tsx
+++ src/components/Detail/presenter.tsx
@@ -36,9 +36,57 @@ export const DetailPresenter: React.FC<Props> = (props) => {
             <header className="header">
               <img src="../src/images/logo.svg" className="logo" />
             </header>
-            <img src={detail.image.url} className="detailImg" />
-            <div>{detail.name}</div>
-            <div>{detail.body}</div>
+            <div className="detail">
+              <div className="detailCard">
+                <img src={detail.image.url} className="detailCardImg" />
+                <div className="detailCardText">
+                  <p>{detail.name}</p>
+                  <hr />
+                  <p className="detailCardBody">{detail.body}</p>
+                </div>
+              </div>
+              <div className="detailBody">
+                <div className="detailBodyBasic">
+                  <p>{detail.name}</p>
+                  <p>{detail.body}</p>
+                </div>
+                <div className="detailBodyTerms">
+                  {detail.mileage == "finalStage" ? (
+                    <p>終盤</p>
+                  ) : detail.mileage == "final" ? (
+                    <p>最終</p>
+                  ) : detail.mileage == "secondHalf" ? (
+                    <p>後半</p>
+                  ) : (
+                    <p>中盤</p>
+                  )}
+                  {detail.location == "straight" ? (
+                    <p>直線</p>
+                  ) : detail.location == "corner" ? (
+                    <p>コーナー</p>
+                  ) : detail.location == "unconditional" ? (
+                    <p>無条件</p>
+                  ) : (
+                    <p>特定距離</p>
+                  )}
+                  <p>{detail.trigger}</p>
+                  {detail.additional ? <p>{detail.additional}</p> : <p>-</p>}
+                </div>
+                <div className="detailBodyEffects">
+                  {detail.amountOfVelocity ? (
+                    <p>{detail.amountOfVelocity}m/s</p>
+                  ) : (
+                    <p>-</p>
+                  )}
+                  {detail.amountOfAcceleration ? (
+                    <p>{detail.amountOfAcceleration}m/s^2</p>
+                  ) : (
+                    <p>-</p>
+                  )}
+                  <p>{detail.duration}秒</p>
+                </div>
+              </div>
+            </div>
           </div>
           <footer>©ケイショウコユウ All Rights Reserved.</footer>
         </div>
